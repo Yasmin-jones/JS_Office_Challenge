280056466bcc4b2307c66b5440a86f753c28f70c
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-11-19 11:17:24 +0000

    first user story completed

---
+++ spec/meetingSpec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+class Meeting{
+
+    constructor(name) {
+        this.name = name 
+    }
+
+    meetingName() {
+        this.name = 'Pink room'; 
+        return this.name; 
+    }
+
+}; 
---
+++ src/meeting.js
@@ -0,0 +1,31 @@
+describe("Meeting", function() {
+
+    'use strict';
+
+    let meeting; 
+
+    beforeEach(() => {
+        meeting = new Meeting();
+      });
+
+      it('Meeting rooms have a name', () => {
+          expect(meeting.meetingName()).toEqual('Pink room');
+
+      });
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
